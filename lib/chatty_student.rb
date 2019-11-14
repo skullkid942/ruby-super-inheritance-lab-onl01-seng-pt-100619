add21c236550d68f68dfc20aeb3e83a1b4f678bb
Bethany Holley <chocolatechipfaced@gmail.com>
2019-11-14 15:18:55 +0000

    Automatically backed up by Learn

--- lib/chatty_student.rb
+++ lib/chatty_student.rb
@@ -7,7 +7,7 @@ class ChattyStudent < Student
   
   def raise_hand
     super
-    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
+    puts "Pick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\nPick me!\n"
   end
   
 end
